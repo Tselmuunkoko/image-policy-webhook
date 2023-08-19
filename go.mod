73ae13b311715e9a8a872625da6122b8bec325c4
Tselmuunzaya <tselmuunkoko@gmail.com>
2023-08-19 22:48:08 +0800

    Added: Logger and Env var

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.25.0 // indirect
 	golang.org/x/net v0.13.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
--- server.go
+++ server.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"encoding/json"
 	"regexp"
-	"sort"
 	"io/ioutil"
 	"k8s.io/api/imagepolicy/v1alpha1"
+	"os"
+	"go.uber.org/zap"
+	"strings"
 )
+
+var logger = zap.Must(zap.NewDevelopment())
+var whitelist = []string{}
 
 const (
 	dockerImagePattern     = `^(?P<registry>.+?\/)?(?P<project>[\w\-]+)\/(?P<image>[\w\-]+):(?P<tag>[\w\-.]+)$`
 	dockerImageHashPattern = `^(?P<registry>.+?\/)?(?P<project>[\w\-]+)\/(?P<image>[\w\-]+)@(?P<hash>sha256:[\w]+)?$`
 )
 
-var whitelist = []string{"harbor.it.org/"}
-
 func checkWhiteList(registry string) bool {
-	index := sort.SearchStrings(whitelist, registry)
-	return !(index < len(whitelist) && whitelist[index] == registry)
+	for _, el := range whitelist {
+		if el == registry {
+			return false
+		}
+	}
+	return true
 }
 
 func splitDockerImage(imageStr string) (registry, project, image, tag, hash string, err error) {
@@ -53,14 +59,13 @@ func splitDockerImage(imageStr string) (registry, project, image, tag, hash stri
 		hash = matchMap["hash"]
 	} else {
 		err = fmt.Errorf("invalid image format")
+		logger.Error("invalid image format")
 	}
 	return
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -72,16 +77,26 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	reqBody, err := ioutil.ReadAll(r.Body)
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	logger.Debug("Request URL: " + scheme + "://" + r.Host + r.RequestURI)
+	for key, values := range r.Header {
+		logger.Debug(key + ": " + strings.Join(values, ", "))
+	}
+	logger.Debug("Request body: " + string(reqBody))
 	if err != nil {
-		log.Printf("server: could not read request body: %s\n", err)
+		logger.Error("server: could not read request body:" + err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	var imageReview v1alpha1.ImageReview
 	err = json.Unmarshal(reqBody, &imageReview)
+
 	if err != nil {
-		log.Println("Error in JSON data:", err)
+		logger.Error("Error in JSON data:" + err.Error())
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
@@ -91,21 +106,21 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	for _, container := range imageReview.Spec.Containers {
 		registry, project, image, tag, hash, err := splitDockerImage(container.Image)
 		if err != nil {
-			log.Println("Error:", err)
-			fmt.Println("Couldn't pass the requirement")
+			logger.Error("Error:" + err.Error())
+			logger.Info("Couldn't pass the requirement")
 			w.WriteHeader(http.StatusOK)
 			jsonData, _ := json.Marshal(resultReview)
 			w.Write(jsonData)
 			return
 		}
 		images = true
-		fmt.Println("Registry:", registry)
-		fmt.Println("Project:", project)
-		fmt.Println("Image:", image)
-		fmt.Println("Tag:", tag)
-		fmt.Println("Hash:", hash)
+		logger.Debug("Registry:" + registry)
+		logger.Debug("Project:" + project)
+		logger.Debug("Image:" + image)
+		logger.Debug("Tag:" + tag)
+		logger.Debug("Hash:" + hash)
 		if tag == "" || tag == "latest" || checkWhiteList(registry) {
-			fmt.Println("Couldn't pass the requirement")
+			logger.Info("Couldn't pass the requirement")
 			w.WriteHeader(http.StatusOK)
 			jsonData, _ := json.Marshal(resultReview)
 			w.Write(jsonData)
@@ -116,19 +131,29 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	resultReview.Status.Allowed = images
 	jsonData, err := json.Marshal(resultReview)
 	if err != nil {
-		log.Println("Error:", err)
+		logger.Error("Error:"+ err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
+	logger.Info("Passed the requirement")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonData)
 }
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	port := os.Getenv("PORT")
+	list := os.Getenv("WHITE_LIST")
+	whitelist = strings.Split(list, ",")
+	fmt.Printf("Starting server at port %s\n", port)
+
+	if os.Getenv("DEBUG") == "false" {
+		logger = zap.Must(zap.NewProduction())
+	}
+	defer logger.Sync()
+	logger.Info("Logger initialized")
+	logger.Debug("WhiteList: " + strings.Join(whitelist, " | "))
+	if err := http.ListenAndServe(":" + string(port), nil); err != nil {
+		logger.Fatal(err.Error())
 	}
 }
